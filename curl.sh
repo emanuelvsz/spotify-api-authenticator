421697eca9dda6be85bf9c22c47f9bed1930fabf
emanuelvsz <usermane13@gmail.com>
2023-09-07 21:55:30 -0300

    updating curl.sh

--- curl.sh
+++ curl.sh
@@ -1,3 +1,3 @@
 curl --request GET \
   --url https://api.spotify.com/v1/me/top/tracks \
-  --header 'Authorization: Bearer BQCc-hjvoSIVgaJ8aXVPqqejzsX9vjugDt1H6dx-4s4V-lGZjKTooUg1-no9mRQS6pyP1RUn0fzBKtCcPe9nRRBfTom0BuS8DnoUt6oyu95C9vyq2I3ukb-Dm0qkDW2hBLXLdZgMkb5-EK_aCTd4P3oUTM-OZK0XdYH6LO1OQAbnaH-Gn8lwYYcw_6xl9MaKlsjdXkEWcUMvMX06EVQPWsAw40qf'
+  --header 'Authorization: Bearer <token>'
